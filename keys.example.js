d02556a48f0a25538ada70464c771cf719e8b175
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2021-12-08 09:13:46 -0800

    Add Example key

---
+++ keys.example.js
@@ -0,0 +1,4 @@
+module.exports = {
+	publicKeyString: "-----BEGIN PUBLIC KEY-----\nPASTE_PUBLIC_KEY_HERE\n-----END PUBLIC KEY-----",
+	privateKeyString: "-----BEGIN PRIVATE KEY-----\nnPASTE_PRIVATE_KEY_HERE\n-----END PRIVATE KEY-----",
+}
