10f64c5b9b76f7730c5e4e20554c7bc9c17b9302
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2021-12-08 09:12:15 -0800

    Using RSA key in a hidden file

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 const crypto = require("crypto");
 const { Buffer } = require("buffer");
-
-const privateKeyString = null;
-const publicKeyString = null;
+const { privateKeyString, publicKeyString } = require("./keys");
 
 exports.handler = async (event) => {
   const { body } = event;
-	
-	const privateKey = crypto.createPrivateKey(privateKeyString);
+
+  const privateKey = crypto.createPrivateKey(privateKeyString);
   const publicKey = crypto.createPublicKey(publicKeyString);
-  
+
   const keypair = { privateKey, publicKey };
 
-	const signature = crypto.sign(null, Buffer.from(body), keypair.privateKey);
-	const signatureBase64 = signature.toString("base64");
-
+  const signature = crypto.sign(null, Buffer.from(body), keypair.privateKey);
+  const signatureBase64 = signature.toString("base64");
 
   const response = {
     statusCode: 200,
     body: JSON.stringify({
       signatureBase64,
-      body
-    })
+      body,
+    }),
   };
-  
+
   return response;
 };
